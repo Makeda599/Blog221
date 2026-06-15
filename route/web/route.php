08b243fe53e5544b77e50f6a5defefd0a76687a9
makeda <adm670in@gmail.com>
2026-06-15 00:52:59 +0000

    fix

--- route/web/route.php
+++ route/web/route.php
@@ -14,12 +14,59 @@ function routes(){
 function gestionController(){
     $routes = routes();
     $controller = $_REQUEST["controller"] ?? "articles";
-
+    $page = $_REQUEST["page"] ?? null;
+    $user = $_SESSION["user"]["role"] ?? null;
     if(!array_key_exists($controller, $routes)){
         echo "controleur introuvable";
         return;
     }
-    if(array_key_exists($controller,$routes)){
-        require_once($routes[$controller]);
+
+    if(!$user){
+        $pagesPubliques = [
+            "articles" => ["accueil","detailArticle"],
+            "auth" => ["login", "inscription"]
+        ];
+
+        if(array_key_exists($controller, $pagesPubliques) && in_array($page, $pagesPubliques[$controller])){
+            require_once($routes[$controller]);
+            return;
+        } else {
+            require_once($routes["articles"]);
+            return;
+        }
     }
+         
+   if($user == "admin"){
+       require_once($routes[$controller]);
+       return;
+   }
+     $pageUser =[];
+    if($user == "lecteur"){
+        $pageUser = [
+            "articles" =>["accueil","detailArticle",],
+            "auth" => ["logOut"],
+            "commentaires" => ["ajoutCommentaire"],
+            "signalArticles" => ["ajoutSignalArticle"],
+            "signalCommentaires" => ["ajoutSignalCommentaires"],
+        ] ;
+        }
+        if($user == "auteur"){
+             $pageUser = [
+            "articles" =>["accueil","detailArticle","ajoutArticle","articleAuteur" , "detailArticle"],
+            "auth" => ["logOut","auteurDashboard"],
+            "commentaires" => ["ajoutCommentaire"],
+            "signalArticles" => ["ajoutSignalArticle"],
+            "signalCommentaires" => ["ajoutSignalCommentaires"],
+        ] ;
+};
+if(array_key_exists($controller,$pageUser) && (in_array($page,$pageUser[$controller])) ){
+            require_once($routes[$controller]);
+            return;
+        }else{
+            echo "Accés interdit";
+            return;
+        }
+   
+   
+  
 }

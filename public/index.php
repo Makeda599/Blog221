4f166142d68d9e3864282cc582dfb4f835d9e5e6
makeda <adm670in@gmail.com>
2026-06-17 12:41:53 +0000

    .gitignore

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 define("ROOT", (str_replace("public","",$_SERVER['DOCUMENT_ROOT'])));
-define("WEBROOT","http://localhost:8005/");
+define("WEBROOT", "http://" . $_SERVER['HTTP_HOST'] . "/");
+
+// define("WEBROOT","http://localhost:8005/");
 
 require_once(ROOT."db/config.php");
 require_once(ROOT."db/helpers.php");

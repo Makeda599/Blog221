b744081da17a4d84b29c3e37460196c5e5251d7e
makeda <adm670in@gmail.com>
2026-06-19 12:10:16 +0000

    fix:ROOT

--- public/index.php
+++ public/index.php
@@ -2,7 +2,9 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+// define("ROOT", (str_replace("public","",$_SERVER['DOCUMENT_ROOT'])));
 define("ROOT", (str_replace("public","",$_SERVER['DOCUMENT_ROOT'])));
+
 
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
 define("WEBROOT", $protocol . $_SERVER['HTTP_HOST'] . "/");
